24c2832eddad2f44082e6b28760ad3ef2bcd315e
Esteban <55922257+s-posito@users.noreply.github.com>
2021-05-28 23:55:41 +0200

    Add Vote for Member

--- src/com/entity/person/Member.java
+++ src/com/entity/person/Member.java
@@ -1,14 +1,17 @@
 package com.entity.person;
 
 import com.entity.admin.Organisation;
+import com.entity.admin.OrganisationDB;
 import com.system.Report;
 import com.veggie.pet.Tree;
 import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.MutablePair;
+import sun.awt.image.ImageWatched;
 
 
 import java.sql.Array;
 import java.time.LocalDate;
-import java.util.ArrayList;
+import java.util.*;
 import java.sql.Date;
 
 public class Member extends Person {
@@ -17,6 +20,7 @@ public class Member extends Person {
     private ArrayList<Tree> treeNominations;
     private float CurrentAccount;
     private ArrayList<ImmutablePair<LocalDate, Float>> contributionList = new ArrayList<>();
+    private Queue<Tree> votes = new LinkedList<>();
 
     // Constructeur
     public Member(String name, String familyName, Date dateOfBirth, String address, Date lastRegistrationDate,
@@ -57,6 +61,27 @@ public class Member extends Person {
         return contributionList;
     }
 
+    public String getTextVotes() {
+        return String.format("[%s] a voté pour les arbres : " + getIdVotes(), this.getName());
+    }
+
+    public Queue<Tree> getVotes() {
+        return votes;
+    }
+
+    private StringBuilder getIdVotes (){
+        StringBuilder s = new StringBuilder();
+        int cpt = 0;
+        for(Tree t : votes){
+            if(cpt<4)
+                s.append(t.getTreeID() + " - ");
+            else
+                s.append(t.getTreeID());
+            cpt++;
+        }
+        return s;
+    }
+
     // Setter
     public void setContributionList(ArrayList<ImmutablePair<LocalDate, Float>> contributionList) {
         this.contributionList = contributionList;
@@ -77,20 +102,36 @@ public class Member extends Person {
         return success;
     }
 
-    public void vote(Tree choice){
-        /* Pour la première action, l’association permet chaque année `a chacun de ses membres de nominer
-        jusqu’`a 5 arbres. 1 La liste effectivement transmise `a la municipalit´e, limit´ee elle aussi `a 5 arbres pour
-        l’association, est obtenue en identifiant les arbres ayant obtenu le plus de voix des membres de l’association.
-        2 La transmission de cette liste se fait une fois par an en fin d’exercice budg´etaire. Toutefois, la
-        r´eception ´eventuelle de la notification d’une classification d’arbre remarquable par le service munipal des
-        espaces verts peut se faire `a tout moment. */
+    public void vote(Tree ...trees){
+        for(Tree t : trees){
+            if(votes.size() >= 5){
+                // Peut etre ajouter le nouveau vote (id de l'arbre + prénom) au profit du plus ancient pour prévenir l'utilisateur (id de l'arbre + prénom)
+                System.out.println(String.format("\n[%s] Do you really want to replace you oldest vote ? " +
+                        "(You already voted for 5 Trees)", this.getName()));
+                
+                Scanner sc = new Scanner(System.in);
+                String answer;
+                do{
+                    System.out.println("\n\tVote Replace : yes or no ?");
+                    answer = sc.next();
+                    answer = answer.toLowerCase();
+                    System.out.println("answer=" + answer);
+                } while (!(answer.equals("yes") || answer.equals("no")));
+                if (answer.equals("yes")){
+                    votes.poll();
+                    votes.offer(t);
+                }
+            }
+            else{
+                votes.add(t);
+            }
+        }
     }
 
     public void submitReport(Report r){
 
     }
 
-    // on imagine que le membre s'est connecté avant et que nous sommes sûrs que ce soit bien lui
     protected void getMyData(Organisation organisation){
         // fonction qui vérifie que le membre en question est bien présent dans l'organisation
         //probablement une fonction dans la classe organisation telle que :
@@ -104,9 +145,12 @@ public class Member extends Person {
     @Override
     public String toString() {
         StringBuilder memberSTB = new StringBuilder(String.format("[Member INFO]\n"));
+        memberSTB.append("{\n");
         memberSTB.append("\tName : " + getName() + "\n");
         memberSTB.append("\tFamily Name : " + getFamilyName() + "\n");
         memberSTB.append("\tPayed Contribution ? " + isPayedContribution() + "\n");
+        memberSTB.append("\tVote : " + getIdVotes() + "\n");
+        memberSTB.append("}\n");
 
         return memberSTB.toString();
     }
@@ -117,11 +161,44 @@ public class Member extends Person {
         Member m2 = new Member("Houssem", "Mahmoud", new Date(1998,03,30), "Somewhere not far from Tunis",
                 new Date(2021,04,23), false, 5000);
 
+        Tree t1 = new Tree(147179, "Marronnier", 150, 15, "hippocastanum",
+                "Aesculus", "Adulte", "CIMETIERE DU PERE LACHAISE / AVENUE DES THUYAS / DIV 86",
+                new Float[]{(float)48.8632712288,(float)2.39435673087}, false);
+
+        Tree t2 = new Tree(1, "Marronnier", 150, 15, "hippocastanum",
+                "Aesculus", "Adulte", "CIMETIERE DU PERE LACHAISE / AVENUE DES THUYAS / DIV 86",
+                new Float[]{(float)48.8632712288,(float)2.39435673087}, false);
+
+        Tree t3 = new Tree(2, "Marronnier", 150, 15, "hippocastanum",
+                "Aesculus", "Adulte", "CIMETIERE DU PERE LACHAISE / AVENUE DES THUYAS / DIV 86",
+                new Float[]{(float)48.8632712288,(float)2.39435673087}, false);
+
+        Tree t4 = new Tree(3, "Marronnier", 150, 15, "hippocastanum",
+                "Aesculus", "Adulte", "CIMETIERE DU PERE LACHAISE / AVENUE DES THUYAS / DIV 86",
+                new Float[]{(float)48.8632712288,(float)2.39435673087}, false);
+
+        Tree t5 = new Tree(4, "Marronnier", 150, 15, "hippocastanum",
+                "Aesculus", "Adulte", "CIMETIERE DU PERE LACHAISE / AVENUE DES THUYAS / DIV 86",
+                new Float[]{(float)48.8632712288,(float)2.39435673087}, false);
+
+        Tree t6 = new Tree(5, "Marronnier", 150, 15, "hippocastanum",
+                "Aesculus", "Adulte", "CIMETIERE DU PERE LACHAISE / AVENUE DES THUYAS / DIV 86",
+                new Float[]{(float)48.8632712288,(float)2.39435673087}, false);
+
+        Tree t7 = new Tree(6, "Marronnier", 150, 15, "hippocastanum",
+                "Aesculus", "Adulte", "CIMETIERE DU PERE LACHAISE / AVENUE DES THUYAS / DIV 86",
+                new Float[]{(float)48.8632712288,(float)2.39435673087}, false);
+
         System.out.println(m1.equals(m2));
         System.out.println(m1.toString());
         m1.payContribution(500);
         System.out.println(m1.toString());
 
         System.out.println(m1.getContributionList());
+
+        m1.vote(t1,t2,t3,t4,t5);
+
+        m1.vote(t6,t7);
+        System.out.println(m1.toString());
     }
 }
