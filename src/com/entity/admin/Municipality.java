b1f6ed37d07e8a4b1392c26d6489c53ec1c6f403
Esteban <55922257+s-posito@users.noreply.github.com>
2021-05-30 00:24:29 +0200

    Add JavaDoc to Municipality

--- src/com/entity/admin/Municipality.java
+++ src/com/entity/admin/Municipality.java
@@ -17,6 +17,11 @@ public class Municipality extends Entity {
     private Map<Integer, Integer> votedTrees;
 
     // Getter
+
+    /**
+     * Permet d'obtenir un clone de la liste d'arbre de la municipalité
+     * @return
+     */
     public ArrayList<Tree> getTrees() {
         return (ArrayList<Tree>) trees.clone();
     }
@@ -28,6 +33,12 @@ public class Municipality extends Entity {
 
     }
 
+    /**
+     * Constructeur d'une municipalité
+     * @param name le nom de la municipalité
+     * @param address l'adresse de la municipalité
+     * @param pathToFile le chemin du fichier .csv correspondant à la liste d'arbre de la municipalité
+     */
     public Municipality(String name, String address, String pathToFile) {
         super(name);
         this.address = address;
@@ -35,10 +46,18 @@ public class Municipality extends Entity {
         this.trees = ParserCSV.parseDataFromCSVFile(fileName);
     }
 
+    /**
+     * Permet d'obtenir l'adresse d'une municipalité
+     * @return l'adresse d'une municipalité
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * Méthode modélisant la classification d'arbres les plus votés.
+     * @param votedTrees la map des arbres les plus votés par les membres d'une association
+     */
     public void setVotedTrees(Map<Integer, Integer> votedTrees) {
         this.votedTrees = votedTrees;
         votedTrees.forEach((k,v) ->
@@ -52,6 +71,11 @@ public class Municipality extends Entity {
         });
     }
 
+    /**
+     * Méthode modélisant l'ajout d'un arbre a la liste d'arbre d'une municipalité
+     * @param tree l'arbre à ajouter
+     * @return un booléen modélisant le succès de l'ajout
+     */
     boolean addTreeToTrees(Tree tree){
         if (!trees.contains(tree)){
             trees.add(tree);
@@ -60,10 +84,19 @@ public class Municipality extends Entity {
         return false;
     }
 
+    /**
+     * Méthode modélisant la suppression d'un arbre de la liste d'arbre d'une municipalité
+     * @param tree l'arbre à supprimer
+     * @return un booléen modélisant le succès de la suppression
+     */
     boolean removeTreeFromTrees(Tree tree){
         return trees.remove(tree);
     }
 
+    /**
+     * Méthode modélisant la classification d'un arbre
+     * @param tree l'arbre à classifier
+     */
     public void classifyTree(Tree tree) {
         if (!tree.isRemarkable()) {
             tree.setRemarkable(true);
@@ -74,6 +107,11 @@ public class Municipality extends Entity {
         }
     }
 
+    /**
+     * Méthode modélisant la classification d'un arbre
+     * @param tree l'arbre à classifier
+     * @param time la date de la classification
+     */
     public void classifyTree(Tree tree, LocalDateTime time) {
         if (!tree.isRemarkable()) {
             tree.setRemarkable(true);
@@ -83,6 +121,4 @@ public class Municipality extends Entity {
             System.err.println("Tree already remarkable");
         }
     }
-
-
 }
